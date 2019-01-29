cf7f864bb7287dc3bc9c2572cb3ca0549f172b93
Dima <40443779+kolodichyuk@users.noreply.github.com>
2019-01-29 17:16:46 +0200

    the experimental class

---
+++ Xml serializer/Song.cs
@@ -0,0 +1,22 @@
+namespace Serializer {
+    class Song {
+        [ShOneXml("Attribute")]
+        public string Id;          // id song
+
+        [ShOneXml("Element")]
+        public string Performen;   // name of the performen
+
+        [ShOneXml("Element")]
+        public string Name;        // name of the song
+
+        //[ShOneXml("Element")]    if there is no attribute, then the field will be sterilized as an element
+        public string Duration;    // song duration
+
+        public Song(string Id, string Performen, string Name, string Duration) {
+            this.Id = Id;
+            this.Performen = Performen;
+            this.Name = Name;
+            this.Duration = Duration;
+        }
+    }
+}
